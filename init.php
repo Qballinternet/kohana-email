89d62d6f20109cb756252cb8477d9532128e9a79
Maurice van der Star <maurice@qball.nl>
2013-07-12 13:24:32 +0200

    Fixed undefined constant and updated swift

--- init.php
+++ init.php
@@ -1,6 +1,6 @@
 <?php defined('SYSPATH') or die('No direct script access.');
 
 // Load Swiftmailer
-if (!USES_COMPOSER) {
+if ( ! defined('USES_COMPOSER') OR ! USES_COMPOSER) {
     include Kohana::find_file('vendor', 'swiftmailer/lib/swift_required', 'php');
 }
